d91905cff371c2980122bf700e6f6d40c0c13246
Marie Andersson <marie.andersson2303@gmail.com>
2020-03-27 10:16:33 +0100

    Install wp event manager

--- public/themes/wu19/functions.php
+++ public/themes/wu19/functions.php
@@ -18,11 +18,12 @@ add_filter('show_admin_bar', '__return_false');
 
 // Enqueue styles and scripts the right way.
 add_action('wp_enqueue_scripts', function () {
-    wp_deregister_script('jquery');
+    // NOTE: Seems like WP Event Manager plugin need jquery loaded in head
+    // wp_deregister_script('jquery');
+    // wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', '', '', true);
+    // wp_enqueue_script('jquery');
     wp_enqueue_style('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css');
-    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', '', '', true);
     wp_register_script('bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js', '', '', true);
-    wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap');
 
     // Enque compiled assets
